c2e522024a97dd5df523a7dd596fc8873ff695fd
x-ant-ie <muchadashantel02@gmail.com>
2023-05-08 10:12:15 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,34 @@
+
+// Declare a constant variable named "PI" and assign it the value of 3.14
+
+const PI = 3.14;
+console.log(PI);
+
+// Use template literals to create a string that says "My name is <your name> and I am <your age> years old."
+
+var myName = 'Gamu';
+var myAge = '20';
+const Person = `My name is ${myName} and I am ${myAge} years old.`
+
+console.log(Person);
+
+//Write an arrow function that takes in two numbers as parameters and returns their sum
+
+const nums = (num1,num2) => num1+num2;
+console.log(nums(3,7));
+
+// Create a function called sum that takes in two parameters, a and b, and returns their sum.
+
+ function sum(a, b){
+    return a + b;
+ };
+
+ console.log(sum(3,5))
+
+/* Create a variable name and assign it the value "John". Using template literals, 
+create a new string that says "Hello, John!" and assign it to a variable called greeting */
+
+  let name ="John";
+  let greeting = `Hello, ${name}!`
+
+  console.log(greeting);
